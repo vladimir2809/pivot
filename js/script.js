f7c3dffe6a1945c1cd3f06f65c2bdadceea3754c
vladimir2809 <46346862+vladimir2809@users.noreply.github.com>
2022-04-05 09:20:03 +0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -4,10 +4,12 @@ var mouseOffsetX=0;
 var mouseOffsetY=0;
 var pi=3.1415926;
 var x=100;
-var y=100;
+var y=263;
 var numLineSelect=null;// выбранная конечность
 var oldAngle=null;
 var flagAngle=false;
+var flagDragHuman=false;
+var maxNumFrame=0;
 var Line={
     x:null,
     y:null,
@@ -51,7 +53,14 @@ dataLine={
 }
 var arrHumanLine=[
 ];
-
+var Frame={
+    x:100,
+    y:100,
+    angleArr:[],
+    lineArr:[],
+    numFrame:0,
+};
+var arrFrame=[];
 window.addEventListener('load', function () {
     preload();
     create();
@@ -76,14 +85,26 @@ function create()
     //setOffsetMousePosXY((window.innerWidth - canvas.width)/2,
     //                        (window.innerHeight - canvas.height)/2);
 //    setOffsetMousePosXY(canvas.x,canvas.y);
-    updateLineHuman();
+    updateLineHuman(0.5);
+    addFrame(dataLine.angleArr);
+}
+function addFrame(angleArr)
+{
+    let frame=clone(Frame);
+    for (let i=0;i<angleArr.length;i++)
+    {
+        frame.angleArr.push(angleArr[i]);
+    }
+    frame.numFrame=maxNumFrame;
+    maxNumFrame++;
+    arrFrame.push(frame); 
 }
 function setOffsetMousePosXY(x,y)// устонавить смешения координаат для прицелевания так как экран начинается не в 0 0
 {
     mouseOffsetX=x;
     mouseOffsetY=y;
 }
-function addLineInHuman(x,y,angle,length)// добавить линию 
+function calcLineInHuman(x,y,angle,length)// добавить линию 
 {
     var line=clone(Line);
     
@@ -93,11 +114,12 @@ function addLineInHuman(x,y,angle,length)// добавить линию
     line.y=y;
     line.x1=Math.cos(pi*(line.angle)/180)*line.length+line.x;
     line.y1=Math.sin(pi*(line.angle)/180)*line.length+line.y;
-    addPointData(line.x1,line.y1);
-    arrHumanLine.push(line);
+    //addPointData(line.x1,line.y1);
+    return line;
+    //arrHumanLine.push(line);
     
 }
-function updateLineHuman()// обновить линии скелета
+function updateLineHuman(scale=1)// обновить линии скелета
 {
     while (dataLine.pointArr.length>0)
     {
@@ -108,41 +130,54 @@ function updateLineHuman()// обновить линии скелета
         arrHumanLine.splice(0,1);
     }
     addPointData(x,y);
-    addLineInHuman(x,y,dataLine.angleArr[0],dataLine.lengthArr[0])//1
+    let arrLine=calcArrLine(scale);
+    for (let i=0;i<arrLine.length;i++)
+    {
+        arrHumanLine.push(arrLine[i]);
+        addPointData(arrLine[i].x1,arrLine[i].y1);
+    }
+}
+function calcArrLine(scale=1)
+{
+    let arrLine=[];
+    arrLine.push(calcLineInHuman(x,y,dataLine.angleArr[0],
+                    dataLine.lengthArr[0]*scale))//1
     //2
-    addLineInHuman(x,y,dataLine.angleArr[1],
-                    dataLine.lengthArr[1]);
+    arrLine.push(calcLineInHuman(x,y,dataLine.angleArr[1],
+                    dataLine.lengthArr[1]*scale));
     //3
-    addLineInHuman(x,y,dataLine.angleArr[2],
-                    dataLine.lengthArr[2]);
-                    addLineInHuman(arrHumanLine[1].x1,arrHumanLine[1].y1,
+    arrLine.push(calcLineInHuman(x,y,dataLine.angleArr[2],
+                    dataLine.lengthArr[2]*scale));
+    ///4
+    arrLine.push(calcLineInHuman(arrLine[1].x1,arrLine[1].y1,
                     filtrAngle(dataLine.angleArr[3]),
-                    dataLine.lengthArr[3]);
+                    dataLine.lengthArr[3]*scale));
     //5
-    addLineInHuman(arrHumanLine[2].x1,arrHumanLine[2].y1,
+    arrLine.push(calcLineInHuman(arrLine[2].x1,arrLine[2].y1,
                     filtrAngle(dataLine.angleArr[4]),
-                    dataLine.lengthArr[4]);
+                    dataLine.lengthArr[4]*scale));
     //6
-    addLineInHuman(arrHumanLine[0].x1,arrHumanLine[0].y1,
+    arrLine.push(calcLineInHuman(arrLine[0].x1,arrLine[0].y1,
                     filtrAngle(dataLine.angleArr[5]),
-                    dataLine.lengthArr[5]);
+                    dataLine.lengthArr[5]*scale));
     //7
-    addLineInHuman(arrHumanLine[0].x1,arrHumanLine[0].y1,
+   arrLine.push( calcLineInHuman(arrLine[0].x1,arrLine[0].y1,
                     filtrAngle(dataLine.angleArr[6]),
-                    dataLine.lengthArr[6]);
+                    dataLine.lengthArr[6]*scale));
     //8
-    addLineInHuman(arrHumanLine[5].x1,arrHumanLine[5].y1,
+   arrLine.push( calcLineInHuman(arrLine[5].x1,arrLine[5].y1,
         filtrAngle(dataLine.angleArr[7]),
-                    dataLine.lengthArr[7]);
+                    dataLine.lengthArr[7]*scale));
                     
     //9
-    addLineInHuman(arrHumanLine[6].x1,arrHumanLine[6].y1,
+    arrLine.push(calcLineInHuman(arrLine[6].x1,arrLine[6].y1,
         filtrAngle( dataLine.angleArr[8]),
-                    dataLine.lengthArr[8]);
+                    dataLine.lengthArr[8]*scale));
     //10
-    addLineInHuman(arrHumanLine[0].x1,arrHumanLine[0].y1,
+    arrLine.push(  calcLineInHuman(arrLine[0].x1,arrLine[0].y1,
         filtrAngle(dataLine.angleArr[9]),
-                    dataLine.lengthArr[9])
+                    dataLine.lengthArr[9]*scale));
+    return arrLine;
 }
 function drawAll()
 {
@@ -189,34 +224,58 @@ function drawAll()
         context.fill();
         context.stroke();
     }
+    context.beginPath();
+    context.moveTo(1,canvas.height/2);
+    context.lineTo(canvas.width,canvas.height/2 );
+    context.stroke();
 }
-
+function drawFrame()
+{
+    
+}
 function update()
 {
     let mX=Math.trunc(mouseX-mouseOffsetX);
     let mY=Math.trunc(mouseY-mouseOffsetY);
-    updateLineHuman();
+    updateLineHuman(0.5);
     if (checkMouseLeft())// если нажата левая кнопка мыши
     {
-        for (let i=1;i<dataLine.pointArr.length;i++)
+        for (let i=0;i<dataLine.pointArr.length;i++)
         {
             let dist=calcDist(dataLine.pointArr[i].x,dataLine.pointArr[i].y,mX,mY);
-            console.log(dist)
-            if (dist<=6 && numLineSelect==null) //если кликнули в суставы скелета
+            if (i==0)
             {
-                
-                numLineSelect=i-1;
-                oldAngle=angle;
+                if (dist<6)
+                {
+                    flagDragHuman=true;
+                    
+                }
+            }
+            else
+            {
+                console.log(dist)
+                if (dist<=6 && numLineSelect==null && flagDragHuman==false) //если кликнули в суставы скелета
+                {
+
+                    numLineSelect=i-1;
+                    oldAngle=angle;
+                }
             }
             
-        }     
+        } 
+        
     }
     else// если отпустили кнопку мыши
     {
         numLineSelect=null;
         flagAngle=false;
+        flagDragHuman=false;
     }  
-    
+    if (flagDragHuman==true)
+    {
+        x=mX;
+        y=mY;
+    }
     if (numLineSelect!=null)// если выбран сустав
     {
        let angle=angleIm(arrHumanLine[numLineSelect].x,arrHumanLine[numLineSelect].y,
@@ -225,7 +284,7 @@ function update()
         {1;
             if (flagAngle==true)
             {
-                updateAngle(numLineSelect,angle);
+                updateAngle(numLineSelect,angle,oldAngle);
                 
             }
             else 
@@ -238,7 +297,7 @@ function update()
         arrHumanLine[numLineSelect].select=true;
     }
 }
-function updateAngle(n,angle)// функция которая обновляет углы скелета по номеру
+function updateAngle(n,angle,oldAngle)// функция которая обновляет углы скелета по номеру
 {
     switch (n)
     {
